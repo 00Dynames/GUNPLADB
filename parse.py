2a0e7839759fa53363a0d5e994b00fb05ed66f37
Dennis <dbunadi@gmail.com>
2019-05-26 22:46:11 +1000

    dump output to json files

--- parse.py
+++ parse.py
@@ -1,4 +1,4 @@
-import sys, re
+import sys, re, json
 from bs4 import BeautifulSoup
 
 
@@ -12,31 +12,43 @@ for i in range(11):
 for line in table:
     number = line
     number = number[5:8] 
-    print("number: " + str(number))
+    #print("number: " + str(number))
     
     name = table.readline()
     name = re.split("[<>]", name)
-    print("name: " + str(name[-3]))
+    #print("name: " + str(name[-3]))
     
     series = table.readline()
     series = re.split("[<>]", series)
-    print("series: " + str(series[-3]))
+    #print("series: " + str(series[-3]))
 
     price = table.readline()
     price = price.rstrip().split(" ")
-    print("price: " + str(price[-1]))
+    #print("price: " + str(price[-1]))
 
     release = table.readline()
     release = release.rstrip().split(" ")
-    print("release: " + " ".join(release[-2:]))
+    #print("release: " + " ".join(release[-2:]))
     
     description = table.readline()
-    soup = BeautifulSoup(description.rstrip())
-    print("description: " + soup.get_text())
+    soup = BeautifulSoup(description.rstrip(), features="html.parser")
+    #print("description: " + soup.get_text())
 
     table.readline()
     table.readline()
 
-    print()
+    result.append({
+        'name': str(name[-3]),
+        'id': int(number) if re.match('[0-9]+', number) else 'N/A',
+        'series': str(series[-3]),
+        'price': str(price[-1]),
+        'release': " ".join(release[-2:]),
+        'description': soup.get_text(),
+        'grade': "MG"
+    })
+
+    #print()
     
 table.close()
+
+print(json.dumps(result))
